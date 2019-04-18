6e7e2b5d7683c9597412f11c62cc7154f6160bec
kkinzeyy <34689935+kkinzeyy@users.noreply.github.com>
2019-04-18 18:12:57 -0400

    Update seeds.sql

--- DB/seeds.sql
+++ DB/seeds.sql
@@ -1,4 +1,7 @@
 --INSERT INTO burgers(burger_name, is_favorite) VALUES('',false);
-INSERT INTO burgers(burger_name, is_favorite) VALUES('corinary bypass',false);
-INSERT INTO burgers(burger_name, is_favorite) VALUES('classic',false);
-INSERT INTO burgers(burger_name, is_favorite) VALUES('double double',true);
+INSERT INTO restaurant_burger(burger_name, is_favorite) VALUES('corinary bypass',false);
+INSERT INTO restaurant_burger(burger_name, is_favorite) VALUES('classic',false);
+INSERT INTO restaurant_burger(burger_name, is_favorite) VALUES('double double',true);
+INSERT INTO restaurant_burger(burger_name, is_favorite) VALUES('triple corinary bypass',false);
+INSERT INTO restaurant_burger(burger_name, is_favorite) VALUES('double cheese',false);
+INSERT INTO restaurant_burger(burger_name, is_favorite) VALUES('swiss',true);
